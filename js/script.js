a4f1fea866790894791987f4f7eb89033876594b
alyssia-fung <124389558+alyssia-fung@users.noreply.github.com>
2023-04-12 16:20:07 +0000

    fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Alyssia fung All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Alyssia fung
+// Created on: Mar 2023
 // This file contains the JS functions for index.html
+
+/**
+ * This function calculates the volume of a sphere
+ */
+"use strict"
+
+function calculate() {
+  // input
+  const radius = parseInt(document.getElementById("radius").value)
+
+  // process
+  const volume = 4/3 * Math.PI * radius**3
+
+  // output
+  document.getElementById("volume").innerHTML =
+    "The volume is: " + volume.toFixed(2) + " cm³"
+}
